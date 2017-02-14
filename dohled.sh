f6de15bf97cde14cb12be136510fad7d99755b89
Your Name <you@example.com>
2017-02-14 10:18:39 +0100

    V 1.3 ÚTERÝ

--- dohled.sh
+++ dohled.sh
@@ -10,3 +10,29 @@ echo "Přijatá data: "$rx
 
 tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
 echo "Přijatá data: "$tx 
+
+
+echo "<html>" >rehacek.html
+
+echo "<body>">>rehacek.html 
+
+echo $cas>>rehacek.html 
+
+echo "<br>">>rehacek.html 
+
+echo $ip>>rehacek.html 
+
+echo "<br>">>rehacek.html 
+
+echo $rx>>rehacek.html 
+
+echo "<br>">>rehacek.html 
+
+echo $tx>>rehacek.html 
+
+echo "<br>">>rehacek.html 
+
+echo "</body>" >>rehacek.html
+
+echo "</html>" >>rehacek.html
+
