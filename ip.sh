88e27f7a183f4deacf2c4f874e64dcb329b5e5ea
Your Name <you@example.com>
2017-02-14 10:03:35 +0100

    V 1.2 ÚTERÝ

---
+++ ip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cas=date
+echo "Aktuální čas"$cas
+ip=`ifconfig | grep Všesměr | awk -F ":" '{printf$2}' | awk -F " " '{print $1}'`
+echo "Moje ip:" $ip
+
+rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
+echo "Přijatá data: "$rx
+
+tx='ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
+echo "Přijatá data: "$tx 
