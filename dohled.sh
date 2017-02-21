15ce922f5dd2bcba9365ee53e5031ac4b615e1ea
Your Name <you@example.com>
2017-02-21 10:18:39 +0100

    V 1.2 výpis

--- dohled.sh
+++ dohled.sh
@@ -1,38 +1,17 @@
 #!/bin/bash
-
 cas=`date +%T`
-echo "Aktuální čas:"$cas
-ip=`ifconfig | grep Všesměr | awk -F ":" '{printf$2}' | awk -F " " '{print $1}'`
-echo "Moje ip:" $ip
-
+ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}' | awk -F " " '{print $1}'`
 rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
-echo "Přijatá data: "$rx
-
 tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
-echo "Přijatá data: "$tx 
+disk=`df -h | grep sda1 | awk -F " " '{print $2}'`
+vdisk=`df -h | grep sda1 | awk -F " " '{print $5}'`
 
 
-echo "<html>" >rehacek.html
-
-echo "<body>">>rehacek.html 
-
-echo $cas>>rehacek.html 
-
-echo "<br>">>rehacek.html 
-
-echo $ip>>rehacek.html 
-
-echo "<br>">>rehacek.html 
-
-echo $rx>>rehacek.html 
-
-echo "<br>">>rehacek.html 
-
-echo $tx>>rehacek.html 
-
-echo "<br>">>rehacek.html 
-
-echo "</body>" >>rehacek.html
-
-echo "</html>" >>rehacek.html
+echo "Aktuální čas: "$cas >> /home/student/Plocha/Skripty/index.html
+echo "Moje IP: "$ip >> /home/student/Plocha/Skripty/index.html
+echo "Přijatá data: "$rx >> /home/student/Plocha/Skripty/index.html
+echo "Odeslaná data: "$tx >> /home/student/Plocha/Skripty/index.html
+echo "Velikost disku:"$disk>> /home/student/Plocha/Skripty/index.html
+echo "Využití disku:"$vdisk >> /home/student/Plocha/Skripty/index.html
+echo "<br><br>" >> /home/student/Plocha/Skripty/index.html
 
