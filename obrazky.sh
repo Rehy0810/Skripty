4c1fe02a83a3694ec93c41aedc47d0f83d4eaccf
Your Name <you@example.com>
2017-03-13 08:37:24 +0100

    Pondelni verze

---
+++ obrazky.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+function MENU {
+	echo "||||||||||||||||||||||MENU|||||||||||||||||||||||"
+	echo "|(1) konverze obrazku na jiny format	      |"
+	echo "|(2) zmena kvality obrazku na libovolnou hodnotu|"
+	echo "|(3) zmena rozmeru obrazku na libovolny rozmer  |"
+	echo "|(4) otoceni obrazku o libovolny uhel           |"
+	echo "|(5) efekt charcoal                             |"
+	echo "|(6) efekt implode                              |"
+	echo "|||||||||||||||||||||||||||||||||||||||||||||||||"
+	echo $volba
+
+}
+
+function efekty {
+	echo "|||||||||||||||||||||||||"
+	echo "||||| (1)Charchoal ||||||"
+	echo "|||||  (2)Iplode   ||||||"
+	echo "|||||||||||||||||||||||||"
+
+}
+
+function kvalita {
+	echo ""
+	echo ""
+	echo ""
+	echo ""
